79fc318267a5f3ce88a6930d33ad2c1c43a0b619
Vincere1st <vincerecaro@gmail.com>
2019-04-18 21:50:54 +0200

    add golden eggs

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 16:07
+ * User: Vincent Ostyn
+ * Date: 18/04/19
+ * Time: 14:07
  * PHP version 7
  */
 
@@ -20,7 +20,7 @@ class ItemController extends AbstractController
 
 //
 //    /**
-//     * Display item listing
+//     * Display Random eggs listing
 //     *
 //     * @return string
 //     * @throws \Twig\Error\LoaderError
@@ -34,17 +34,20 @@ class ItemController extends AbstractController
 
         session_start();
 
-        if(empty($_SESSION)){
-
-            $rand_keys = array_rand($eggs,3);
+        if (empty($_SESSION)) {
+            $h = 6; //numbers of eggs
+            $rand_keys = array_rand($eggs, $h);
             $_SESSION['eggs'] = $rand_keys;
-            $j= count($rand_keys);
-            for($i=0;$i<$j;$i++){
-
+            $k= $h-3; // $h-3 equal of numbers of gold eggs
+            for ($i=0; $i<$k; $i++) {
                 $_SESSION['eggs'][$i] = $eggs[$rand_keys[$i]];
+            }
+            for($j = $k; $j< $h; $j++) {
+                $_SESSION['eggsgold'][$j] = $eggs[$rand_keys[$j]];
             }
         }
 
-        return $this->twig->render('Item/index.html.twig', ['eggs' => $_SESSION['eggs']]);
+        return $this->twig->render('Item/index.html.twig', ['eggs' => $_SESSION['eggs'],
+                                                                  'eggsgold' => $_SESSION['eggsgold']]);
     }
 }
--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: sylvain
- * Date: 07/03/18
- * Time: 18:20
+ * User: Vincent Ostyn
+ * Date: 18/04/19
+ * Time: 16:14
  * PHP version 7
  */
 
@@ -16,10 +16,7 @@ class ItemManager extends AbstractManager
 
     public function eggs()
     {
-        $client = new GuzzleHttp\Client([
-                'base_uri' => 'http://easteregg.wildcodeschool.fr',
-            ]
-        );
+        $client = new GuzzleHttp\Client(['base_uri' => 'http://easteregg.wildcodeschool.fr',]);
 
         $response = $client->request('GET', '/api/eggs');
 
@@ -30,7 +27,6 @@ class ItemManager extends AbstractManager
         $eggs = $body->getContents();
         $eggs = GuzzleHttp\json_decode($eggs, true);
         return $eggs;
-
     }
 
 
@@ -48,48 +44,4 @@ class ItemManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-
-//
-//    /**
-//     * @param array $item
-//     * @return int
-//     */
-//    public function insert(array $item): int
-//    {
-//        // prepared request
-//        $statement = $this->pdo->prepare("INSERT INTO $this->table (`title`) VALUES (:title)");
-//        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-//
-//        if ($statement->execute()) {
-//            return (int)$this->pdo->lastInsertId();
-//        }
-//    }
-//
-//
-//    /**
-//     * @param int $id
-//     */
-//    public function delete(int $id): void
-//    {
-//        // prepared request
-//        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
-//        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-//        $statement->execute();
-//    }
-//
-//
-//    /**
-//     * @param array $item
-//     * @return bool
-//     */
-//    public function update(array $item):bool
-//    {
-//
-//        // prepared request
-//        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
-//        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-//        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-//
-//        return $statement->execute();
-//    }
 }
