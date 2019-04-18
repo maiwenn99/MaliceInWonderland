2683f5c15f00190e5a3f0665ca150ae49facf19a
maiwenn99 <maiwenngay99@gmail.com>
2019-04-18 18:42:38 +0200

    final design home and over

---
+++ src/Controller/TestController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maiwenn
+ * Date: 18/04/2019
+ * Time: 15:13
+ */
+
+namespace App\Controller;
+
+
+class TestController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function test()
+    {
+        return $this->twig->render('Home/test.html.twig');
+    }
+
+}
