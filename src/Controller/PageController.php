8642335b6904f0076143304a0d62623f082ba917
maiwenn99 <maiwenngay99@gmail.com>
2019-04-18 22:27:52 +0200

    ordered

---
+++ src/Controller/PageController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maiwenn
+ * Date: 18/04/2019
+ * Time: 22:19
+ */
+
+namespace App\Controller;
+
+
+class PageController extends AbstractController
+{
+
+    public function enter()
+    {
+        return $this->twig->render('Page/enter.html.twig');
+    }
+
+    public function page11()
+    {
+        return $this->twig->render('Page/page1-1.html.twig');
+    }
+
+    public function page12()
+    {
+        return $this->twig->render('Page/page1-2.html.twig');
+    }
+
+    public function page21()
+    {
+        return $this->twig->render('Page/page2-3.html.twig');
+    }
+
+    public function page22()
+    {
+        return $this->twig->render('Page/page2-3.html.twig');
+    }
+
+    public function page23()
+    {
+        return $this->twig->render('Page/page2-3.html.twig');
+    }
+
+    public function page24()
+    {
+        return $this->twig->render('Page/page2-3.html.twig');
+    }
+
+    public function over()
+    {
+        return $this->twig->render('Page/gameOver.html.twig');
+    }
+
+}
