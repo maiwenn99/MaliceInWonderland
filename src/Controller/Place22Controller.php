927c1731427e8d060741381bc78f25cf4ae916f2
Vanina Diot <vanina.diot@gmail.com>
2019-04-19 00:39:11 +0200

    Creation place 2-2

---
+++ src/Controller/Place22Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class Place22Controller extends AbstractController
+{
+    public function place22()
+    {
+        return $this->twig->render('Home/place22.html.twig');
+    }
+}
