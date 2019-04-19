8971240719a6e89f9fb919f161fceeccb427be72
Vincere1st <vincerecaro@gmail.com>
2019-04-19 03:17:30 +0200

    add session destroy in success/gameover page

--- public/index.php
+++ public/index.php
@@ -5,10 +5,9 @@
  * Date: 11/10/17
  * Time: 14:01
  */
-if (!isset($_SESSION))
-{
-    session_start();
-}
+
+session_start();
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
