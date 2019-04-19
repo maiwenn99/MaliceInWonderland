57e14960e5d0c56a94fbc9a12fbbbdbbad7f1a76
maiwenn99 <maiwenngay99@gmail.com>
2019-04-19 08:58:49 +0200

    final design commit

--- config/db.php
+++ config/db.php
@@ -19,6 +19,6 @@
  *
  */
 define('APP_DB_USER', 'root');
-define('APP_DB_PWD', 'rootJicespsmred-45000');
+define('APP_DB_PWD', 'maille99');
 define('APP_DB_HOST', 'localhost');
 define('APP_DB_NAME', 'wild_db_quest');
